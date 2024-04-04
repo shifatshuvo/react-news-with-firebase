1c6b03d748bb6fdfecef37a4c60b45aa2e52e2da
shifatshuvo <knock.shifat@gmail.com>
2024-04-04 17:54:44 +0600

    after basic setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuyyvbpqkeJTb77jjNYZw6IsyTPnF64S0",
+  authDomain: "react-dragon-new-auth.firebaseapp.com",
+  projectId: "react-dragon-new-auth",
+  storageBucket: "react-dragon-new-auth.appspot.com",
+  messagingSenderId: "328443682614",
+  appId: "1:328443682614:web:1b4232f6454f90f2eb1d97"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
